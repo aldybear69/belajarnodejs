f0b5c370a3f88ad4b55d9b5c071f6bac9e641615
aldybear69 <aldybear69@gmail.com>
2022-06-21 13:12:10 +0700

    menambahkan view engine ejs

---
+++ express/index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// set app bahwa menggunakan template engine EJS
+app.set("view engine", "ejs");
+// karena project expressjs ini views folder tidak di root maka ganti lokasi views ejs nya
+app.set("views", path.join(__dirname, "./views"));
+
+const contohDataMahasiswa = [
+  {
+    nama: "Danila",
+    umur: "20",
+    noHP: "081634",
+  },
+  {
+    nama: "Agnes",
+    umur: "30",
+    noHP: "0813434",
+  },
+  {
+    nama: "Enzy",
+    umur: "20",
+    noHP: "086114",
+  },
+];
+
+app.get("/", (req, res) => {
+  // res.send("Hello World!");
+  // res.sendFile("./pages/index.html", { root: __dirname }); // menggunakan file pada folder pages
+  res.render("index", {
+    nama: "B34R",
+    title: "Home Page",
+    contohDataMahasiswa,
+  }); // parameter objek yg akan dikirimkan ke view ejs
+});
+app.get("/about", (req, res) => {
+  // res.send("About Page!");
+  // res.sendFile("./pages/about.html", { root: __dirname }); // menggunakan file pada folder pages
+  res.render("about", { title: "About Page" });
+});
+app.get("/contact", (req, res) => {
+  // res.send("Contact Page!");
+  // res.sendFile("./pages/contact.html", { root: __dirname }); // menggunakan file pada folder pages
+  res.render("contact", { title: "Contact Page" });
+});
+
+// contoh jika menggunakan parameter
+app.get("/product/:id/category/:idCat", (req, res) => {
+  res.send(
+    `Product ID : ${req.params.id} <br>Category ID : ${req.params.idCat}`
+  );
+  // test ke alamat ini http://localhost:3000/product/satu/category/5
+});
+
+// contoh jika menggunakan query
+app.get("/product/:id", (req, res) => {
+  res.send(
+    `Product ID : ${req.params.id} <br>Category ID : ${req.query.category}`
+  );
+  // test ke alamat ini http://localhost:3000/product/satu?category=baju
+});
+
+// use digunakan diakhir karena sifatnya akan merespon
+// alamat apa saja yg tidak ada menjadi respon yg di definisikan di app.use
+// maka hati2 ketika menyimpan method use ini di baris awal sebelum route lainnya
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>ERR 404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("index.js");
+// ! TENTANG VAR dan LET dalam JAVASCRIPT
+// * Dalam javascript itu sistem yang dianut adalah Function Scope,
+// * kalau di bahasa lain adalah Block Scope
+// * sehingga variable var dalam javascript akan berperilaku seperti ini :
+
+function main() {
+  for (var i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  // var i dipanggil di luar fungsi loop forr masih tetap bisa mendapatkan nilainya
+  console.log(i);
+}
+main();
+
+// * kalau di bahasa lain tidak akan bisa mengakses var i akan undefined error
+// * seperti contoh pada script dart di bawah ini :
+// void main() {
+//   for(var i = 0; i < 10; i++){
+//     print(i);
+//   }
+//     print(i);
+// }
+
+// * maka dalam javascript itu diakalin pake yg namanya konsep IIFE / SIAF
+// * IIFE = Immediately Invoked Function Expression
+// * SIAF = Self Invoking Anonymous Function
+// * seperti di bawah ini :
+
+(function test() {
+  for (var j = 0; j < 10; j++) {
+    console.log(j);
+  }
+})();
+// var j dipanggil di luar fungsi sudah tidak bisa mendapatkan nilainya
+// console.log(j); //error is not defined
+
+// * maka dari itu dianjurkan untuk menggunakan atau mengganti semua VAR dengan LET
+// * karena perilaku LET sudah Block Scope sama seperti bahasa lain
+// * contohnya seperti ini :
+function tes() {
+  for (let k = 0; k < 10; k++) {
+    console.log(k);
+  }
+  // var i dipanggil di luar fungsi loop forr masih tetap bisa mendapatkan nilainya
+  // console.log(k); // akan error is not defined karena di panggil dari luar
+}
+tes();
